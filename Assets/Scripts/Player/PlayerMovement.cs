c2da9212f31893025c25fc21c6dd14b2fc9c549b
ProfL3304 <ProfL3304@lab.aca>
2023-09-29 15:26:17 -0500

    Mejora de manejo de velocidad
    Se utiliza Vector3

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private Vector2 Speed = new Vector2(-4f, 4f);
+    private Vector3 Speed = new Vector3(4f, 0f, 4f);
 
     private Rigidbody rb;
     private Vector2 moveDir;
@@ -21,7 +21,7 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector3(
             moveDir.x * Speed.x,
             0f,
-            moveDir.y * Speed.y
+            moveDir.y * Speed.z
         );
     }
 
